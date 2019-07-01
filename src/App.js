7c5f4d5a08e0622bd80a377013ae9ad772b43d01
Lavanya1510 <lavanya.dhasarathan@gmail.com>
2019-07-01 18:17:34 +0530

    api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+export class App extends Component {
+state = {
+  isLoading: true,
+  users: [],
+  error: null
+}
+fetchUsers() {
+ 
+   fetch(`https://jsonplaceholder.typicode.com/users`)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   
+    .then(response => response.json())
+    
+    .then(data =>
+      this.setState({
+        users: data,
+        isLoading: false,
+      })
+    )
+ 
+    .catch(error => this.setState({ error, isLoading: false }));
+}
+componentDidMount() {
+  this.fetchUsers();
 }
 
-export default App;
+ 
+
+
+  render() {
+    return (
+      <React.Fragment>
+         
+         <h1>Random User</h1>
+         {this.state.error ? <p>{this.state.Error.message}</p> : null}
+    
+      {!this.state.isLoading ? (
+       
+        this.state.users.map(user => {
+          const { username, name, email } = user;
+          return (
+            <div key={username}>
+              <p>Name: {name}</p>
+              <p>Email Address: {email}</p>
+              <hr />
+            </div>
+          );
+        })
+      // If there is a delay in data, let's let the user know it's loading
+      ) : (
+        <h3>Loading...</h3>
+      )}
+      </React.Fragment>
+    )
+  }
+}
+
+export default App
+
